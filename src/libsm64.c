c8b08ce705198d4926493deeacb98ecb7968767a
Dylan Grinberg <dylanpdx@gmail.com>
2021-09-04 16:34:33 +0100

    Teleport function

--- src/libsm64.c
+++ src/libsm64.c
@@ -225,6 +225,20 @@ SM64_LIB_FN void sm64_mario_delete( int32_t marioId )
     obj_pool_free_index( &s_mario_instance_pool, marioId );
 }
 
+SM64_LIB_FN void sm64_mario_teleport( int32_t marioId, float x, float y, float z )
+{
+    if( marioId >= s_mario_instance_pool.size || s_mario_instance_pool.objects[marioId] == NULL )
+    {
+        DEBUG_PRINT("Tried to change position of non-existant Mario with ID: %u", marioId);
+        return;
+    }
+    global_state_bind( ((struct MarioInstance *)s_mario_instance_pool.objects[ marioId ])->globalState );
+
+    gMarioState->pos[0]=x;
+    gMarioState->pos[1]=y;
+    gMarioState->pos[2]=z;
+}
+
 SM64_LIB_FN uint32_t sm64_surface_object_create( const struct SM64SurfaceObject *surfaceObject )
 {
     uint32_t id = surfaces_load_object( surfaceObject );
