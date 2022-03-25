7ca5d045efe89aaaf0fdc5496d110633dacceb01
Dylan <dylanpdx@gmail.com>
2022-03-25 23:57:02 +0000

    Vibri Model

--- src/model_handler.c
+++ src/model_handler.c
@@ -3,6 +3,7 @@
 #include "decomp/model_steve/geo.inc.h"
 #include "decomp/model_alex/geo.inc.h"
 #include "decomp/model_necoarc/geo.inc.h"
+#include "decomp/model_vibri/geo.inc.h"
 // models done
 #include "decomp/engine/geo_layout.h"
 #include "model_handler.h"
@@ -19,6 +20,7 @@ void initModels(struct AllocOnlyPool *pool){
     modelPointers[MODEL_STEVE] = (void*)steve_geo_ptr;
     modelPointers[MODEL_ALEX] = (void*)alex_geo_ptr;
     modelPointers[MODEL_NECOARC] = (void*)necoarc_geo_ptr;
+    modelPointers[MODEL_VIBRI] = (void*)vibri_geo_ptr;
 
     for (int i = 0;i<ModelsUsed;i++){
         model_nodes[i]=process_geo_layout( pool, modelPointers[i] );
