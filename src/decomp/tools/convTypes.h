67cc01bc3641f46570e0482a3eaeb863dfda2e33
Dylan <dylanpdx@gmail.com>
2022-04-08 17:31:37 +0100

    CTL instrument and percussion parsing

--- src/decomp/tools/convTypes.h
+++ src/decomp/tools/convTypes.h
@@ -3,6 +3,30 @@
 #define TYPE_CTL 1
 #define TYPE_TBL 2
 
+struct Sound{
+    unsigned int sample_addr;
+    float tuning;
+};
+
+struct Drum{
+    unsigned char release_rate;
+    unsigned char pan;
+    unsigned char loaded;
+    unsigned char pad;
+    struct Sound snd;
+    unsigned int env_addr;
+};
+
+struct Instrument{
+    unsigned char loaded;
+    unsigned char normal_range_lo;
+    unsigned char normal_range_hi;
+    unsigned char release_rate;
+    unsigned int env_addr;
+    struct Sound sound_lo;
+    struct Sound sound_med;
+    struct Sound sound_hi;
+};
 
 struct CTL
 {
--- src/decomp/tools/convUtils.c
+++ src/decomp/tools/convUtils.c
@@ -21,14 +21,39 @@ struct seqFile* parse_seqfile(unsigned char* seq){ /* Read SeqFile data */
 
     // test
     for (int i = 0; i < bankCount; i++){
-        parse_ctl_data(seqFile->seqArray[i].offset);
+        parse_ctl_data(seq+(seqFile->seqArray[i].offset));
     }
     // test
 
     return seqFile;
 }
 
+struct Sound parse_sound(unsigned char* sound){
+    struct Sound* snd = (struct Sound*)sound;
+    snd->sample_addr=read_u32_be(sound);
+    snd->tuning = (float)read_f32_be(sound+4);
+    // if sample_addr is 0 then the sound is null
+}
+
+struct Drum parse_drum(unsigned char* drum){ /* Read Drum data */
+    struct Drum* drumData = (struct Drum*)drum;
+    // first 4 bytes don't need reading
+    parse_sound(&drumData->snd);
+    drumData->env_addr=read_u32_be(drum+sizeof(struct Sound));
+    
+}
+
+struct Instrument parse_instrument(unsigned char* instrument){
+    struct Instrument* inst = (struct Instrument*)instrument;
+    // first 4 bytes don't need reading
+    inst->env_addr=read_u32_be(instrument+4);
+    parse_sound(&inst->sound_lo);
+    parse_sound(&inst->sound_med);
+    parse_sound(&inst->sound_hi);
+}
+
 struct CTL* parse_ctl_data(unsigned char* ctlData){
+    
     struct CTL* ctl = (struct CTL*)malloc(sizeof(struct CTL));
     #pragma region Parse CTL header
     ctl->numInstruments = read_u32_be(ctlData);
@@ -38,13 +63,21 @@ struct CTL* parse_ctl_data(unsigned char* ctlData){
     #pragma endregion
     // header parsed, now read data
     ctl->drum_pointers= (int*)malloc(ctl->numDrums * sizeof(int));
+    int drumTablePtr = read_u32_be(ctlData + 16);
     for (int i = 0; i < ctl->numDrums; i++){
-        ctl->drum_pointers[i] = read_u32_be(ctlData + 16 + i * 4);
+        ctl->drum_pointers[i] = read_u32_be(ctlData + drumTablePtr+16 + i * 4);
+        struct Drum d = parse_drum(ctlData+ctl->drum_pointers[i]+16);
     }
-    ctl->instrument_pointers = (void*)malloc(ctl->numInstruments * sizeof(void*));
+    // parse instrument data
+    ctl->instrument_pointers = (int*)malloc(ctl->numInstruments * sizeof(int));
+    int instTablePtr = 16+4;
     for (int i = 0; i < ctl->numInstruments; i++){
-        ctl->instrument_pointers[i] = ctlData + 16 + ctl->numDrums * 4 + i * 4;
+        ctl->instrument_pointers[i] = read_u32_be(ctlData + instTablePtr + i * 4);
+        if (ctl->instrument_pointers[i] == 0)
+            continue;
+        struct Instrument inst = parse_instrument(ctlData+ctl->instrument_pointers[i]+16);
     }
+    // 
 
     return ctl;
 }
