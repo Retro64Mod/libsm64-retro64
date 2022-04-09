03969e38d8a9741ffba0153fb5a116e4be9b1b0c
Mario <Jgmario9501@gmail.com>
2022-04-09 13:56:30 -0600

    Fixed healing while underwater and crawling under a 1 gap block
    Fixed some bugs with crawling under a 1 gap block and healing while underneath a block while submerged in water.

--- src/decomp/game/mario_step.c
+++ src/decomp/game/mario_step.c
@@ -294,7 +294,7 @@ static s32 perform_ground_quarter_step(struct MarioState *m, Vec3f nextPos) {
         return GROUND_STEP_LEFT_GROUND;
     }
 
-    if (floorHeight + 160.0f >= ceilHeight) {
+    if (!(m->action & ACT_FLAG_SHORT_HITBOX) && floorHeight + 160.0f >= ceilHeight) {
         return GROUND_STEP_HIT_WALL_STOP_QSTEPS;
     }
 
