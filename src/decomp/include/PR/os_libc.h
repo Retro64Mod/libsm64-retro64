70e9d2e23223c6e3826d85755a2496136f787d72
Caleb Elmasri <calebelm@icloud.com>
2022-04-11 10:22:50 -0500

    Added MacOS support for native m1 and intel

--- src/decomp/include/PR/os_libc.h
+++ src/decomp/include/PR/os_libc.h
@@ -2,9 +2,9 @@
 #define _OS_LIBC_H_
 
 #include "ultratypes.h"
-
+#include <strings.h>
 // Old deprecated functions from strings.h, replaced by memcpy/memset.
-extern void bcopy(const void *, void *, size_t);
-extern void bzero(void *, size_t);
+//extern void bcopy(const void *, void *, size_t);
+//extern void bzero(void *, size_t);
 
 #endif /* !_OS_LIBC_H_ */
